b2cd159630b498424fc9c22d3b5c15b192a3455e
foremoreee <yyaroslavyy83@gmail.com>
2024-09-25 21:11:47 +0300

    Add files via upload

--- LabM_1/Task4/MyFirst.java
+++ LabM_1/Task4/MyFirst.java
@@ -1,1 +1,17 @@
+import myfirstpackage.*;
 
+class MyFirstClass {
+    public static void main(String[] s) {
+        MyFirstPackage o = new MyFirstPackage(993, 7);
+        System.out.println(o.PlusNow());
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                o.setFirstNum(i);
+                o.setSecondNum(j);
+                System.out.print(o.PlusNow());
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
